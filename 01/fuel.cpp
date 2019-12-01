2fc46d6ecf5a01ec63e569f1482c08a56220d5ce
Maksim Solovjov <himaxx2@gmail.com>
2019-12-01 10:23:06 +0000

    Solve day 1

---
+++ 01/fuel.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <chrono>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+size_t fuel_requirement(size_t w) {
+    return w / 3 - 2;
+}
+
+size_t fuel_sum(const auto& weights) {
+    return accumulate(begin(weights), end(weights), 0, [](auto acc, auto w) {
+        return acc + fuel_requirement(w);
+    });
+}
+
+//------------------------------------------------------------------------------
+#define TEST_EQ(a, b) { std::cout << #a << " == " << #b << " == " << ((a) == (b)) << std::endl; assert((a) == (b)); }
+
+void test() {
+    TEST_EQ(fuel_requirement(12), 2);
+    TEST_EQ(fuel_requirement(14), 2);
+    TEST_EQ(fuel_requirement(1969), 654);
+    TEST_EQ(fuel_requirement(100756), 33583);
+}
+
+void run() {
+    using namespace std::chrono;
+    high_resolution_clock::time_point t_start = high_resolution_clock::now();
+
+    std::vector<size_t> weights;
+    copy(
+        std::istream_iterator<size_t>(std::cin),
+        std::istream_iterator<size_t>(),
+        back_inserter(weights));
+
+    high_resolution_clock::time_point t_io = high_resolution_clock::now();
+
+    auto answer = fuel_sum(weights);
+
+    high_resolution_clock::time_point t_end = high_resolution_clock::now();
+    auto io_duration = duration_cast<microseconds>(t_io - t_start).count();
+    auto all_duration = duration_cast<microseconds>(t_end - t_start).count();
+
+    std::cout << answer << " in " << all_duration << "us (io: "
+              << io_duration << "us)" << std::endl;
+}
+
+int main() {
+    test();
+    run();
+}
+
